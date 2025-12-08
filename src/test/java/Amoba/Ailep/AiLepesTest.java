a19ef6be5c89d90205134312ea5c4f7c7ef22f2f
Laskovics Lilla <laskovicslillamaria@gmail.com>
2025-12-08 14:01:17 +0100

    Add Mockito

--- src/test/java/Amoba/Ailep/AiLepesTest.java
+++ src/test/java/Amoba/Ailep/AiLepesTest.java
@@ -4,11 +4,40 @@ import Amoba.Board;
 import Amoba.Service.Ai;
 import Amoba.Service.GameService;
 import org.junit.jupiter.api.Test;
+
+import static Amoba.Board.BOARD_SIZE;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-public class AiLepesTest {
+import static org.mockito.Mockito.*;
+@ExtendWith(MockitoExtension.class)
+class AiLepesMockitoTest {
 
-    // --- AI LÉPÉS TESZT ---
+    @Mock
+    Board board;
+
+    @Mock
+    GameService service;
+
+    @InjectMocks
+    Ai ai;
+
+    @Test
+    void testAiMeghivjaALepestValodiBoarddal() {
+
+        GameService service = mock(GameService.class);
+        when(service.nyertesVizsgalat(any())).thenReturn(0); // ne legyen nyerés
+
+        Ai ai = new Ai(service);
+        Board board = new Board();
+
+        ai.aiLep(board);
+
+        verify(service, atLeastOnce()).nyertesVizsgalat(any());
+    }
 
     @Test
     void testAiLepUresHelyre() {
@@ -18,8 +47,8 @@ public class AiLepesTest {
         ai.aiLep(board);
         boolean vanO = false;
         int[][] t = board.getTabla();
-        for (int i = 0; i < Board.BOARD_SIZE; i++) {
-            for (int j = 0; j < Board.BOARD_SIZE; j++) {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
                 if (t[i][j] == 2) {
                     vanO = true;
                     break;
