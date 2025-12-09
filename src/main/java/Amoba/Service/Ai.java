94a139eb1218a62d74543d88c85b37b44115c595
Laskovics Lilla <laskovicslillamaria@gmail.com>
2025-12-09 20:01:40 +0100

    Ai butítása

--- src/main/java/Amoba/Service/Ai.java
+++ src/main/java/Amoba/Service/Ai.java
@@ -33,21 +33,21 @@ public class Ai {
         }
 
         // 2) AI blokkolja a játékost
-        for (int sor = 0; sor < Board.BOARD_SIZE; sor++) {
-            for (int oszlop = 0; oszlop < Board.BOARD_SIZE; oszlop++) {
-                if (board.getTabla()[sor][oszlop] == 0) {
-                    board.getTabla()[sor][oszlop] = 1;
-                    if (service.nyertesVizsgalat(board) == 1) {
-                        board.getTabla()[sor][oszlop] = 0;
-                        board.lep(sor, oszlop, 2);
-                        log.info("AI (O) blokkoló mezőre lépett: [{},{}]", sor, oszlop);
-//                        System.out.println("AI (O) lépett a következő mezőre: [" + sor + "," + oszlop + "]");
-                        return;
-                    }
-                    board.getTabla()[sor][oszlop] = 0;
-                }
-            }
-        }
+//        for (int sor = 0; sor < Board.BOARD_SIZE; sor++) {
+//            for (int oszlop = 0; oszlop < Board.BOARD_SIZE; oszlop++) {
+//                if (board.getTabla()[sor][oszlop] == 0) {
+//                    board.getTabla()[sor][oszlop] = 1;
+//                    if (service.nyertesVizsgalat(board) == 1) {
+//                        board.getTabla()[sor][oszlop] = 0;
+//                        board.lep(sor, oszlop, 2);
+//                        log.info("AI (O) blokkoló mezőre lépett: [{},{}]", sor, oszlop);
+////                        System.out.println("AI (O) lépett a következő mezőre: [" + sor + "," + oszlop + "]");
+//                        return;
+//                    }
+//                    board.getTabla()[sor][oszlop] = 0;
+//                }
+//            }
+//        }
         // 3) különben random lép
         log.info("AI nem talált nyerő vagy blokkoló mezőt -> random lépés");
         superRandom(board);
